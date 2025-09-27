601d4b478ca1d8531b7116e0724f559b4edd0769
@Androi <apamisaran@ccci-tech.com>
2025-09-27 09:56:07 +0800

    server.js done

--- serverings/server/server.js
+++ serverings/server/server.js
@@ -1,15 +1,24 @@
 const express = require('express') //allows usage of express library/commands
 const app = express() // connecting your app while using express commands
+const {faker} = require('@faker-js/faker');
 
 app.listen(8080, ()=>{
     console.log(`Server is running at http://localhost:8080`) //this will simply indicate if the server is up and running
 })
 
-const greetings = ["Hello", "World", "Peace"]
+const greetings = ["Hello", "Hi", "Kumusta"]
 app.get("/api/tryserver",(req,res) => { //initial
     res.json({
         message:"hello world",
         greetings: greetings
+    })
+})
+
+app.get("/api/showperson", (req, res) => {
+    res.json({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        phoneNumber: faker.phone.imei()
     })
 })
 
